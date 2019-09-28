d003e376e58d33c5532dc374305b45df9e027c36
SethFeingold <sfeingold1104@gmail.com>
2019-09-28 12:14:32 -0700

    hideseek works, images could still disappear better

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// // grab the text inside the searchbox upon keyup
+
+// let input = document.getElementById("mySearch");
+
+// input.addEventListener('keyup', function(){
+//     let searchText = input.value.toLowerCase();
+
+//     $('a').each(function(){
+//         let imageCaption = $('a').attr('data-caption').toLowerCase();
+//         if (imageCaption.indexOf(searchText) < 0) {
+//             $(this).addClass('hidden');
+//     }
+//     })
+// });
+
